617f6d769241d92f5339bf28da74ebb0d2a7ac3e
KAKASHII-HATAKE <96489385+KAKASHII-HATAKE@users.noreply.github.com>
2022-03-30 11:40:14 +0530

    Update auraToLWC.js

--- auraToLWC/auraToLWC.js
+++ auraToLWC/auraToLWC.js
@@ -1,4 +1,4 @@
-import { LightningElement,track } from 'lwc';
+import { LightningElement,track, wire } from 'lwc';
 import EXE_BATCH from '@salesforce/apex/BatchApexProgressIndicatorController.executeBatchJob';
 import BATCH_STATUS from '@salesforce/apex/BatchApexProgressIndicatorController.getBatchJobStatus';
 
@@ -6,54 +6,77 @@ export default class AuraToLWC extends LightningElement {
 
     @track progress=0;
     @track id
-    JobItemsProcessed
-    TotalJobItems
-    @track processStatus = 'In Progress';
-
-    
+    @track JobItemsProcessed
+    @track TotalJobItems
+    @track processStatus
 
 
-    exeBatch()
-    {
-       EXE_BATCH().then(Response=>{
-           this.id=Response;
-           console.log('got this response  ',Response);
-           this.progressHandler();
-       }).catch(error=>{
-           console.log('Error in exeBatch : '+error.body.message);
-       })
+       @wire(BATCH_STATUS,{jobID:'$id'})
+       myData({data,error})
+       {
+           if(data)
+           {
+               this.JobItemsProcessed=data.JobItemsProcessed;
+               this.TotalJobItems=data.TotalJobItems;
+               this.processStatus=data.Status;
+               console.log(' AsyncBatch ',data);
+               console.log(' AsyncBatch ',data.Status);
+           }
+           if(error)
+           {
+               console.error(error);
+           }
+       }
 
-       
-    }
+       async  exeBatch()
+       {
+        await  EXE_BATCH().then(Response=>{
+              this.id=Response;
+              console.log('got this response  ',Response);
+             // this.progressHandler(Response);
+          }).catch(error=>{
+              console.log('Error in exeBatch : '+error.body.message);
+          })
 
-    progressHandler(){
-        console.log('progress handler called');
-        console.log('responce id :',this.id);
+        }
+
+
+
+
+//     }
+
+//    async progressHandler(res){
+//         console.log('progress handler called');
+//         console.log('responce id :',this.id);
         
-        BATCH_STATUS({jobID:this.id}).then(response=>{
-            this.TotalJobItems=response.TotalJobItems;
-            this.JobItemsProcessed=response.JobItemsProcessed;
-            if(response.JobItemsProcessed!=null){
-               // this.progress=(response.JobItemsProcessed / response.TotalJobItems) * 100;  //condition if required
-                this.progressBar();
-            }
+//       await  BATCH_STATUS({jobID:res}).then(response=>{
+//             this.TotalJobItems= response.TotalJobItems;
+//             this.JobItemsProcessed=response.JobItemsProcessed;
+//             if(response.JobItemsProcessed!=null){
+//                // this.progress=(response.JobItemsProcessed / response.TotalJobItems) * 100;
+//                 this.progressBar();
+//             }
             
-            console.log(response.TotalJobItems);
-            console.log(response.JobItemsProcessed);
-        }).catch(error=>{
-            console.log('Error in progressHandler : '+error.body.message);
-        })
+//             console.log('total job item : ',response.TotalJobItems);
+//             console.log('job item processed : ',response.JobItemsProcessed);
+//         }).catch(error=>{
+//             console.log('Error in progressHandler : '+error.body.message);
+//         })
         
-    }
+//     }
 
     progressBar() {
         
         this._interval = setInterval(() => {
-            this.progress = this.progress + 5;
-            this.processStatus = 'Processing => ' + this.progress +'/100';
-            if(this.progress === 100) {
+            this.progress = this.progress + this.JobItemsProcessed;
+            this.processStatus = 'Processing => ' + this.progress + ''+this.TotalJobItems;
+            if(this.JobItemsProcessed === this.TotalJobItems) {
                 clearInterval(this._interval);
                 this.processStatus = 'Completed';
+            }
+            else {
+                clearInterval(this._interval);
+                this.processStatus = 'Failed';
             }
         }, 300);
 }
